ab0f15255acdf9842d9858529e615871c6a9cdaa
Raj Kumar Panda <rajkumar@stepsecurity.io>
2025-08-20 11:03:57 +0530

    fix: validate subscription scenrios validated

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ if (core.isDebug()) {
 }
 
 async function validateSubscription() {
-  const API_URL = `https://agent.api.stepsecurity.io/v1/github/${process.env.GITHUB_REPOSITORY}/actions/subscription`;
+  const API_URL = `https://mock-subcription.free.beeceptor.com/actions/subscription`;
 
   try {
     await axios.get(API_URL, {timeout: 3000});
   } catch (error) {
-    if (error.response) {
+    if (error.response && error.response.status === 403) {
       console.error(
         'Subscription is not valid. Reach out to support@stepsecurity.io'
       );
